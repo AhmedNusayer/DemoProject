7247b91a1bdee941abfb8592d70a511c4b9b7e8a
Ahmed Nusayer Ashik <42416199+AhmedNusayer@users.noreply.github.com>
2021-04-06 13:51:00 +0600

    components separated

---
+++ DemoProject/Scripts/profile-info.js
@@ -0,0 +1,14 @@
+﻿Vue.component("profile-info", {
+    props: ["picpath", "name", "username", "email", "address", "phonenumber", "dateofbirth"],
+    data: function () {
+        return {
+            picturePath: this.picpath,
+            Name: this.name,
+            UserName: this.username,
+            Email: this.email,
+            Address: this.address,
+            PhoneNumber: this.phonenumber,
+            DateofBirth: this.dateofbirth
+        }
+    },
+})
---
+++ DemoProject/Scripts/register-company.js
@@ -0,0 +1,40 @@
+﻿Vue.component("register-company", {
+    data: function () {
+        return {
+            CompanyName: "",
+            CompanyLocation: "",
+            CompanyAddress: "",
+            CompanyEmail: "",
+            CompanyType: "",
+            VerificationCode: "",
+            toggle: true
+        }
+    },
+
+    methods: {
+        validateEmail(email) {
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
+        },
+        sendMail() {
+            location.href = 'SendMail/?actions=verifycompany&address=' + this.CompanyEmail
+        },
+        isClicked() {
+            this.toggle = !this.toggle
+            this.sendMail()
+        }
+    },
+
+    computed: {
+        isDisabled() {
+            if (this.CompanyName.length != 0 && this.CompanyLocation.length != 0 && this.CompanyAddress.length != 0 &&
+                this.CompanyEmail.length != 0 && this.CompanyType.length != 0 &&
+                this.validateEmail(this.CompanyEmail)) {
+                return false;
+            } else {
+                return true;
+            }
+        },
+    }
+
+})
