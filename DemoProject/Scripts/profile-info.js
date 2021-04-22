0fb04909301491b35a1c112b2878b73aa7f6f135
Ahmed Nusayer Ashik <42416199+AhmedNusayer@users.noreply.github.com>
2021-04-22 20:27:04 +0600

    added education section

--- DemoProject/Scripts/profile-info.js
+++ DemoProject/Scripts/profile-info.js
@@ -9,7 +9,45 @@
             Email: this.email,
             Address: this.address,
             PhoneNumber: this.phonenumber,
-            DateofBirth: this.dateofbirth
+            DateofBirth: this.dateofbirth,
+            Intro: "",
+            Educations: [],
+            Education: {
+                Institution: "",
+                Degree: "",
+                FieldOfStudy: "",
+                StartDate: "",
+                EndDate: "",
+                Grade: ""
+            },
+            forUpdate: false
         }
     },
+
+    methods: {
+        addEducation() {
+            this.Educations.push({
+                Institution: this.Education.Institution, Degree: this.Education.Degree,
+                FieldOfStudy: this.Education.FieldOfStudy, StartDate: this.Education.StartDate,
+                EndDate: this.Education.EndDate, Grade: this.Education.Grade
+            })
+        },
+        addEducationClicked() {
+            this.forUpdate = false
+            this.Education = {}
+        },
+        deleteEducation() {
+            this.Educations.filter(el => el.Institution != this.Education.Institution &&
+                el.Degree != this.Education.Degree &&
+                el.FieldOfStudy != this.Education.FieldOfStudy &&
+                el.StartDate != this.Education.StartDate &&
+                el.EndDate != this.Education.EndDate &&
+                el.Grade != this.Education.Grade
+            )
+        },
+        getEdu(edu) {
+            this.forUpdate = true
+            this.Education = edu
+        }
+    }
 })
