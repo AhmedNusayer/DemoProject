9e830655d4d9e4b907d017b3363c4c143e100a69
Ahmed Nusayer Ashik <42416199+AhmedNusayer@users.noreply.github.com>
2021-04-26 19:44:48 +0600

    migration script added

--- InfrastructureProject/Migrations/20210426133527_UserProfileUpdated.Designer.cs
+++ InfrastructureProject/Migrations/20210426133527_UserProfileUpdated.Designer.cs
@@ -4,14 +4,16 @@ using InfrastructureProject;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InfrastructureProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20210426133527_UserProfileUpdated")]
+    partial class UserProfileUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
